1870ea5845d44bdcf644f9f1cfb7d19ff90e587b
Jim Eriksson <jim@jaydog.se>
2023-07-14 13:17:06 +0200

    nvim initial

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p ~/.config
+
 rm -f ~/.bash_aliases
 rm -f ~/.bash_logout
 rm -f ~/.bash_profile
@@ -8,14 +10,14 @@ rm -f ~/.gitconfig
 rm -f ~/.git-prompt.sh
 rm -f ~/.inputrc
 rm -f ~/.vim
-rm -f ~/.local
+rm -f ~/.config/nvim
 
-ln -s dotfiles/bash/bash_aliases ~/.bash_aliases
-ln -s dotfiles/bash/bash_logout ~/.bash_logout
-ln -s dotfiles/bash/bash_profile ~/.bash_profile
-ln -s dotfiles/bash/bashrc ~/.bashrc
-ln -s dotfiles/git/gitconfig ~/.gitconfig
-ln -s dotfiles/git/git-prompt.sh ~/.git-prompt.sh
-ln -s dotfiles/bash/inputrc ~/.inputrc
-ln -s dotfiles/vim ~/.vim
-ln -s dotfiles/local ~/.local
+ln -s ~/dotfiles/bash/bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/bash/bash_logout ~/.bash_logout
+ln -s ~/dotfiles/bash/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/bash/bashrc ~/.bashrc
+ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/git/git-prompt.sh ~/.git-prompt.sh
+ln -s ~/dotfiles/bash/inputrc ~/.inputrc
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/nvim ~/.config/nvim
