71801b5d015c8cafc576e77dab0e80412abfde9d
Jim Eriksson <jim@jaydog.se>
2023-09-13 19:34:44 +0200

    newsboat

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ rm -f ~/.inputrc
 rm -f ~/.vim
 rm -f ~/.config/nvim
 rm -rf ~/.config/alacritty
+rm -rf ~/.config/newsboat
 
 ln -s ~/dotfiles/bash/bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/bash/bash_logout ~/.bash_logout
@@ -23,3 +24,4 @@ ln -s ~/dotfiles/bash/inputrc ~/.inputrc
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/alacritty ~/.config/alacritty
+ln -s ~/dotfiles/newsboat ~/.config/newsboat
