8c6649f0fce2ca64367bbf933f8748f9e7be7c30
Jim Eriksson <jim@jaydog.se>
2024-04-28 11:54:01 +0200

    stuff

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.config
 mkdir -p ~/.vim_undo_files
-mkdir -p ~/config/Code/User
+mkdir -p ~/.config/Code/User
 
 rm -f ~/.bash_aliases
 rm -f ~/.bash_logout
@@ -12,13 +12,14 @@ rm -f ~/.gitconfig
 rm -f ~/.git-prompt.sh
 rm -f ~/.inputrc
 rm -f ~/.digrc
-rm -f ~/vim
+rm -f ~/.vim
 rm -f ~/.config/nvim
 rm -rf ~/.config/alacritty
 rm -f ~/.tmux.conf
-rm -f ~/dotfiles/vscode/user/settings.json ~/.config/Code/User/settings.json
-rm -rf ~/dotfiles/vscode/user/snippets ~/.config/Code/User/snippets
-rm -f ~/dotfiles/vscode/user/keybindings.json ~/.config/Code/User/keybindings.json
+rm -f ~/.config/Code/User/settings.json
+rm -rf ~/.config/Code/User/snippets
+rm -f ~/.config/Code/User/keybindings.json
+rm -rf ~/.config/bat
 
 ln -s ~/dotfiles/bash/bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/bash/bash_logout ~/.bash_logout
@@ -35,3 +36,4 @@ ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/vscode/user/settings.json ~/.config/Code/User/settings.json
 ln -s ~/dotfiles/vscode/user/snippets ~/.config/Code/User/snippets
 ln -s ~/dotfiles/vscode/user/keybindings.json ~/.config/Code/User/keybindings.json
+ln -s ~/dotfiles/bat ~/.config/bat
