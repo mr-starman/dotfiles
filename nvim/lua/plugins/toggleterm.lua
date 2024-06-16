098b173fc407d517c72d894b9e3ffa7df0d20309
Jim Eriksson <jim@jaydog.se>
2024-06-16 20:06:48 +0200

    nvim stuff

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,6 +1,7 @@
 return {
-	'akinsho/toggleterm.nvim', version = "*", config = true,
+	"akinsho/toggleterm.nvim",
 	config = function()
+		require("toggleterm").setup()
 		vim.keymap.set('n', '<c-d>', '<c-c><cmd>ToggleTerm<cr>')
 	end
 }
