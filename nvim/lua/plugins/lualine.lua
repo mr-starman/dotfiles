15077a3debe1c38afc65b38fdef9b1e2b9870a71
Jim Eriksson <jim@jaydog.se>
2024-06-23 23:06:48 +0200

    lualine relative path

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,20 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'auto'
-      }})
-  end
+	'nvim-lualine/lualine.nvim',
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	config = function()
+		require('lualine').setup {
+			options = {
+				theme = 'auto',
+			},
+			sections = {
+				lualine_c = {
+					{
+						'filename',
+						file_status = true,   -- displays file status (readonly status, modified status)
+						path = 1              -- 0 = just filename, 1 = relative path, 2 = absolute path
+					}
+				}
+			},
+		}
+	end,
 }
