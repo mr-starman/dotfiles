f30c64746a94c9c279d5a8fd1fa4b93006c35f7f
Jim Eriksson <jim@jaydog.se>
2024-06-23 23:23:55 +0200

    nvim options

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,32 +1,32 @@
+vim.g.have_nerd_font = true
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
 vim.g.mapleader = ','
 vim.g.maplocalleader = ','
-vim.g.have_nerd_font = true
-vim.g.loaded_perl_provider = 0 
-vim.g.loaded_ruby_provider = 0
 vim.g.python3_host_prog = '/bin/python3'
 
-vim.opt.number = true
-vim.opt.mouse = 'a'
-vim.opt.showmode = false
-vim.opt.clipboard = 'unnamedplus'
 vim.opt.breakindent = true
-vim.opt.undofile = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.cursorline = true
+vim.opt.hlsearch = true
 vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.smartindent = true
-vim.opt.signcolumn = 'yes'
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.opt.inccommand = 'split'
 vim.opt.list = false
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = 'split'
-vim.opt.cursorline = true
+vim.opt.mouse = 'a'
+vim.opt.number = true
 vim.opt.scrolloff = 10
-vim.opt.hlsearch = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-
-
+vim.opt.showmode = false
+vim.opt.signcolumn = 'yes'
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.softtabstop = 2
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 2
+vim.opt.timeoutlen = 300
+vim.opt.undofile = true
+vim.opt.updatetime = 250
