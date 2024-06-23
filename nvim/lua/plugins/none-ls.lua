16e00db7ac5575ba6a2e4d52cf046e0a58be20e4
Jim Eriksson <jim@jaydog.se>
2024-06-23 17:08:59 +0200

    go debugging

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,17 +1,19 @@
 return {
-  "nvimtools/none-ls.nvim",
+  'nvimtools/none-ls.nvim',
   config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
+    local null_ls = require 'null-ls'
+    null_ls.setup {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.diagnostics.erb_lint,
         null_ls.builtins.diagnostics.rubocop,
         null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.goimports,
       },
-    })
+    }
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
   end,
 }
