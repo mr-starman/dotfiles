72c66cffd3ae8a335663a73e2fc8f8d165bf2d77
Jim Eriksson <jim@jaydog.se>
2024-06-23 09:11:37 +0200

    vim options and lazy update

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -25,8 +25,8 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 
 
