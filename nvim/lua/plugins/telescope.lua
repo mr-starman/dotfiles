01c5de1fc6a381e61b17d5e1f5fc90240ce021c6
Jim Eriksson <jim@jaydog.se>
2024-06-29 20:49:27 +0200

    telescope fzf

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,32 +4,45 @@ return {
 		tag = '0.1.6',
 		dependencies = {
 			'nvim-lua/plenary.nvim',
+			{ -- If encountering errors, see telescope-fzf-native README for installation instructions
+				'nvim-telescope/telescope-fzf-native.nvim',
+
+				-- `build` is used to run some command when the plugin is installed/updated.
+				-- This is only run then, not every time Neovim starts up.
+				build = 'make',
+
+				-- `cond` is a condition used to determine whether this plugin should be
+				-- installed and loaded.
+				cond = function()
+					return vim.fn.executable 'make' == 1
+				end,
+			},
 		},
 		config = function()
 			pcall(require('telescope').load_extension, 'fzf')
-			local builtin = require("telescope.builtin")
+			require('telescope').load_extension 'ui-select'
+			local builtin = require 'telescope.builtin'
 			vim.keymap.set('n', '<C-p>', builtin.find_files)
 			vim.keymap.set('n', '<leader>b', builtin.buffers)
 			vim.keymap.set('n', '<leader>g', builtin.live_grep)
 			vim.keymap.set('n', '<leader>bl', builtin.current_buffer_fuzzy_find)
-		end
+		end,
 	},
 	{
-		"nvim-telescope/telescope-ui-select.nvim",
+		'nvim-telescope/telescope-ui-select.nvim',
 		config = function()
-			require("telescope").setup({
+			require('telescope').setup {
 				pickers = {
 					find_files = {
 						hidden = true,
 					},
 				},
 				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
+					['ui-select'] = {
+						require('telescope.themes').get_dropdown {},
 					},
 				},
-			})
-			require("telescope").load_extension("ui-select")
-		end
-	}
+			}
+		end,
+	},
 }
