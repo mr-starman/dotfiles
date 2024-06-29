66bd33d7959b1f2e819979021726cb9b2d946324
Jim Eriksson <jim@jaydog.se>
2024-06-29 20:55:24 +0200

    toggleterm size

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -2,6 +2,7 @@ return {
 	'akinsho/toggleterm.nvim',
 	config = function()
 		require('toggleterm').setup {
+			size = 17,
 			open_mapping = [[<c-t>]],
 		}
 		vim.api.nvim_set_keymap('n', '<c-t>', ':ToggleTerm<CR>', { noremap = true, silent = true })
