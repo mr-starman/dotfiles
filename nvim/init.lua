cd76ad567c4ab6d52d9b87b5e3254e27376bc37c
Jim Eriksson <jim@jaydog.se>
2024-06-30 14:01:52 +0200

    lazy rounded corner

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,22 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system {
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
+		lazypath,
+	}
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("vim-options")
-require("key-bindings")
+require 'vim-options'
+require 'key-bindings'
 -- TODO: Move to custom Lazy plug?
-require("cobol-stuff")
-require("lazy").setup("plugins")
+require 'cobol-stuff'
+require('lazy').setup('plugins', {
+	ui = {
+		border = 'rounded',
+	},
+})
