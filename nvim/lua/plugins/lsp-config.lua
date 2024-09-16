b2d5669eb318713d06031e2ecd9692ccb4fb6719
Jim Eriksson <jim@jaydog.se>
2024-09-16 18:49:20 +0200

    lsp stuff

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -39,6 +39,9 @@ return {
 			lspconfig.gopls.setup {
 				capabilities = capabilities,
 			}
+			lspconfig.ts_ls.setup {
+				capabilities = capabilities,
+			}
 			lspconfig.ocamllsp.setup {
 				filetypes = { 'ocaml', 'ocaml.menhir', 'ocaml.interface', 'ocaml.ocamllex', 'reason', 'dune' },
 				root_dir = lspconfig.util.root_pattern('*.opam', 'esy.json', 'package.json', '.git', 'dune-project', 'dune-workspace', '*.ml'),
@@ -49,7 +52,6 @@ return {
 			vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
 			vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
 			vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-			vim.keymap.set('n', '<leader>rn', vim.lsp.buf.code_action, {})
 			vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
 		end,
 	},
