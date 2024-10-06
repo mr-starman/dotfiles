aabacf8f12f07b837827bffd71a56133863058de
Jim Eriksson <jim@jaydog.se>
2024-10-06 19:54:18 +0200

    nvim stuff

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,25 @@
+-- Code Tree Support / Syntax Highlighting
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+  -- https://github.com/nvim-treesitter/nvim-treesitter
+  'nvim-treesitter/nvim-treesitter',
+  event = 'VeryLazy',
+  dependencies = {
+    -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
+  build = ':TSUpdate',
+  opts = {
+    highlight = {
+      enable = true,
+    },
+		indent = { enable = true },
+    auto_install = true, -- automatically install syntax support when entering new file type buffer
+    ensure_installed = {
+      'lua',
+    },
+  },
+  config = function(_, opts)
+    local configs = require 'nvim-treesitter.configs'
+    configs.setup(opts)
+  end,
 }
