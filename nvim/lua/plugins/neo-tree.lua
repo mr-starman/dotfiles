448c356de34c4a79d9d40142179fc5dda3144cf3
Jim Eriksson <jim@jaydog.se>
2024-12-27 18:50:22 +0100

    neo-tree config

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,12 +1,19 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set('n', '<C-E>', ':Neotree filesystem reveal left<CR>')
-  end
+	'nvim-neo-tree/neo-tree.nvim',
+	branch = 'v3.x',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+		'MunifTanjim/nui.nvim',
+	},
+	config = function()
+		vim.keymap.set('n', '<C-E>', ':Neotree filesystem reveal left<CR>')
+		require('neo-tree').setup {
+			filesystem = {
+				filtered_items = {
+					visible = true, -- Ensure all files are visible
+				},
+			},
+		}
+	end,
 }
