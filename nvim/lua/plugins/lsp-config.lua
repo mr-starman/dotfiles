e3034c80fd8f51f62caafc4e0304aeb5fde4f560
Jim Eriksson <jim@jaydog.se>
2024-12-27 10:20:20 +0100

    python lsp stuff

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -70,6 +70,22 @@ return {
           },
         },
       }
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local c = vim.lsp.get_client_by_id(args.data.client_id)
+          if not c then
+            return
+          end
+
+          -- Format the current buffer on save
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            buffer = args.buf,
+            callback = function()
+              vim.lsp.buf.format { bufnr = args.buf, id = c.id }
+            end,
+          })
+        end,
+      })
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', '<leader>gh', vim.lsp.buf.hover, {})
