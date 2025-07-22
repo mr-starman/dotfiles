b0290e43cbce909b66c6a9910ca990b2ed32490a
Jim Eriksson <jim@jaydog.se>
2025-07-22 19:55:26 +0200

    nvim config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,48 +1,49 @@
 return {
-	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.6',
-		dependencies = {
-			'nvim-lua/plenary.nvim',
-			{ -- If encountering errors, see telescope-fzf-native README for installation instructions
-				'nvim-telescope/telescope-fzf-native.nvim',
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.6',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { -- If encountering errors, see telescope-fzf-native README for installation instructions
+        'nvim-telescope/telescope-fzf-native.nvim',
 
-				-- `build` is used to run some command when the plugin is installed/updated.
-				-- This is only run then, not every time Neovim starts up.
-				build = 'make',
+        -- `build` is used to run some command when the plugin is installed/updated.
+        -- This is only run then, not every time Neovim starts up.
+        build = 'make',
 
-				-- `cond` is a condition used to determine whether this plugin should be
-				-- installed and loaded.
-				cond = function()
-					return vim.fn.executable 'make' == 1
-				end,
-			},
-		},
-		config = function()
-			pcall(require('telescope').load_extension, 'fzf')
-			require('telescope').load_extension 'ui-select'
-			local builtin = require 'telescope.builtin'
-			vim.keymap.set('n', '<C-p>', builtin.find_files)
-			vim.keymap.set('n', '<leader>b', builtin.buffers)
-			vim.keymap.set('n', '<leader>g', builtin.live_grep)
-			vim.keymap.set('n', '<leader>bl', builtin.current_buffer_fuzzy_find)
-		end,
-	},
-	{
-		'nvim-telescope/telescope-ui-select.nvim',
-		config = function()
-			require('telescope').setup {
-				pickers = {
-					find_files = {
-						hidden = true,
-					},
-				},
-				extensions = {
-					['ui-select'] = {
-						require('telescope.themes').get_dropdown {},
-					},
-				},
-			}
-		end,
-	},
+        -- `cond` is a condition used to determine whether this plugin should be
+        -- installed and loaded.
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    },
+    config = function()
+      pcall(require('telescope').load_extension, 'fzf')
+      require('telescope').load_extension 'ui-select'
+      local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<C-p>', builtin.find_files)
+      vim.keymap.set('n', '<leader>b', builtin.buffers)
+      vim.keymap.set('n', '<leader>g', builtin.live_grep)
+      vim.keymap.set('n', '<leader>bl', builtin.current_buffer_fuzzy_find)
+    end,
+  },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config = function()
+      require('telescope').setup {
+        pickers = {
+          find_files = {
+            hidden = true,
+            file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+          },
+        },
+        extensions = {
+          ['ui-select'] = {
+            require('telescope.themes').get_dropdown {},
+          },
+        },
+      }
+    end,
+  },
 }
