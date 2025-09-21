06e78fa155aca9bad5c8cf76fe1fe3823a5217c5
Jim Eriksson <jim@jaydog.se>
2025-09-21 12:16:04 +0200

    lsp config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -25,91 +25,82 @@ return {
 			})
 		end,
 	},
-	{
-		'neovim/nvim-lspconfig',
-		dependencies = {
-			{ "folke/neodev.nvim" },
-		},
-		lazy = false,
-		config = function()
-			if vim.g.lsp_config_loaded then
-				return
-			end
-			vim.g.lsp_config_loaded = true
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      { "folke/neodev.nvim" },
+    },
+    lazy = false,
+    config = function()
+      if vim.g.lsp_config_loaded then
+        return
+      end
+      vim.g.lsp_config_loaded = true
 
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-			local lspconfig = require 'lspconfig'
-			lspconfig.tailwindcss.setup {
-				capabilities = capabilities,
-			}
-			lspconfig.solargraph.setup {
-				capabilities = capabilities,
-			}
-			lspconfig.html.setup {
-				capabilities = capabilities,
-			}
-			lspconfig.lua_ls.setup {
-				capabilities = capabilities,
-			}
-			lspconfig.gopls.setup {
-				capabilities = capabilities,
-			}
-			lspconfig.ts_ls.setup {
-				capabilities = capabilities,
-			}
-			lspconfig.csharp_ls.setup {
-				capabilities = capabilities,
-			}
-			lspconfig.taplo.setup {
-				capabilities = capabilities,
-			}
-			lspconfig.bashls.setup {
-				capabilities = capabilities,
-			}
-			lspconfig.pylsp.setup {
-				capabilities = capabilities,
-			}
-			lspconfig.jdtls.setup {
-				capabilities = capabilities,
-			}
-			lspconfig.rust_analyzer.setup {
-				settings = {
-					['rust-analyzer'] = {
-						assist = {
-							importMergeBehavior = 'last',
-							importPrefix = 'by_self',
-						},
-						cargo = {
-							loadOutDirsFromCheck = true,
-						},
-						procMacro = {
-							enable = true,
-						},
-					},
-				},
-			}
-			vim.api.nvim_create_autocmd('LspAttach', {
-				callback = function(args)
-					local c = vim.lsp.get_client_by_id(args.data.client_id)
-					if not c then
-						return
-					end
+      -- Define your servers and their configs
+      local servers = {
+        tailwindcss = { capabilities = capabilities },
+        solargraph  = { capabilities = capabilities },
+        html        = { capabilities = capabilities },
+        lua_ls      = { capabilities = capabilities },
+        gopls       = { capabilities = capabilities },
+        ts_ls       = { capabilities = capabilities },
+        csharp_ls   = { capabilities = capabilities },
+        taplo       = { capabilities = capabilities },
+        bashls      = { capabilities = capabilities },
+        pylsp       = { capabilities = capabilities },
+        jdtls       = { capabilities = capabilities },
+        rust_analyzer = {
+          capabilities = capabilities,
+          settings = {
+            ['rust-analyzer'] = {
+              assist = {
+                importMergeBehavior = 'last',
+                importPrefix = 'by_self',
+              },
+              cargo = {
+                loadOutDirsFromCheck = true,
+              },
+              procMacro = {
+                enable = true,
+              },
+            },
+          },
+        },
+      }
 
-					-- Format the current buffer on save
-					vim.api.nvim_create_autocmd('BufWritePre', {
-						buffer = args.buf,
-						callback = function()
-							vim.lsp.buf.format { bufnr = args.buf, id = c.id }
-						end,
-					})
-				end,
-			})
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-			vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
-			vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
-			vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-			vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
-		end,
-	},
+      -- Register and enable each server
+      for name, cfg in pairs(servers) do
+        vim.lsp.config[name] = cfg
+        vim.lsp.enable(name)
+      end
+
+      -- Autocommands
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local c = vim.lsp.get_client_by_id(args.data.client_id)
+          if not c then
+            return
+          end
+
+          -- Format the current buffer on save
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            buffer = args.buf,
+            callback = function()
+              vim.lsp.buf.format { bufnr = args.buf, id = c.id }
+            end,
+          })
+        end,
+      })
+
+      -- Keymaps
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+    end,
+  },
 }
