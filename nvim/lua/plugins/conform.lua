f08a3376fe1d1020e6c35a4354f02aae514ea6e2
Jim Eriksson <jim@jaydog.se>
2026-07-09 12:35:41 +0200

    lua formatting

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,12 +2,15 @@ return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
   cmd = { "ConformInfo" },
+
   opts = {
     formatters_by_ft = {
       python = { "isort", "black" },
+      lua = { "stylua" },
     },
+
     format_on_save = {
-      timeout_ms = 1000,
+      timeout_ms = 500,
       lsp_format = "fallback",
     },
   },
